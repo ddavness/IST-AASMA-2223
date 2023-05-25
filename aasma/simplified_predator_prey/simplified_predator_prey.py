3b76161bbb59172497171928bb35ecbba3989217
David Duque <david@duqued.net>
2023-05-25 04:50:43 +0100

    Environment updates
    Co-authored-by: Simão Paiva <SimonLimon@users.noreply.github.com>

--- aasma/simplified_predator_prey/simplified_predator_prey.py
+++ aasma/simplified_predator_prey/simplified_predator_prey.py
@@ -1,8 +1,11 @@
 import copy
 import logging
 import random
+import copy
 
+import math
 import numpy as np
+import functools
 
 logger = logging.getLogger(__name__)
 
@@ -15,67 +18,108 @@ from ma_gym.envs.utils.action_space import MultiAgentActionSpace
 from ma_gym.envs.utils.draw import draw_grid, fill_cell, draw_circle, write_cell_text
 from ma_gym.envs.utils.observation_space import MultiAgentObservationSpace
 
-class SimplifiedPredatorPrey(gym.Env):
+class SnakeEnvironment(gym.Env):
 
-    """A simplified version of ma_gym.envs.predator_prey.predator_prey.PredatorPrey
-    Observations do not take into account the nearest cells and an extra parameter (required_captors) was added
+    """
+    A modified version of ma_gym.envs.predator_prey.predator_prey.PredatorPrey to fit a snake game
 
-    See Also
-    --------
-    ma_gym.envs.predator_prey
-
+    grid_shape: (x, y) - The shape of the grid, how big is it? Must be at least 10x10
+    n_agents: int - The number of players/snakes
+    max_steps: int - The maximum number of moves in the game. Defaults to x * y
+    food_equilibrium: (int) -> float - The target number of food points in the environment. Defaults to (3/2)*area^(2/3)
     """
 
     metadata = {'render.modes': ['human', 'rgb_array']}
 
-    def __init__(self, grid_shape=(5, 5), n_agents=2, max_steps=100):
+    def __food_equilibrium_default(self, area):
+        return 3/2 * area^(2/3)
+
+    def __init__(self, grid_shape=(10, 10), n_agents=2, max_steps=None, food_equilibrium=None):
+        if grid_shape[0] < 10 or grid_shape[1] < 10:
+            raise ValueError("Grid must be at least 10x10")
+
         self._grid_shape = grid_shape
         self.n_agents = n_agents
-        self._max_steps = max_steps
+        self._max_steps = max_steps if max_steps is not None else grid_shape[0] * grid_shape[1]
+        self._food_equilibrium_f = food_equilibrium if food_equilibrium is not None else self.__food_equilibrium_default
         self._step_count = None
 
         self.action_space = MultiAgentActionSpace([spaces.Discrete(3) for _ in range(self.n_agents)])
-        self.agent_pos = {_: None for _ in range(self.n_agents)}
         self.agent_body = {_: None for _ in range(self.n_agents)}
 
-        self._base_grid = self.__create_grid()  # with no agents
+        self._base_grid = self.__create_grid()
         self._full_obs = self.__create_grid()
-        # self._agent_dones = [False for _ in range(self.n_agents)]
         self.viewer = None
+        
+        self.alive = None
+        self.body = {_: [] for _ in range(self.n_agents)}  # self.body= a list of all the bodys from agents than are a list on its own[(x1,y1),(x2,y2)]
+        self.direction = {_: random.randint(0, 3) for _ in range(self.n_agents)} # 0 down,1 left, 2 up, 3 right
+
+        self.food = []
 
         self._obs_high = np.tile(self._obs_high, self.n_agents)
         self._obs_low = np.tile(self._obs_low, self.n_agents)
         self.observation_space = MultiAgentObservationSpace(
             [spaces.Box(self._obs_low, self._obs_high) for _ in range(self.n_agents)])
 
+        self.__create_snakes(self.n_agents)
         self.seed()
 
     def simplified_features(self):
 
         current_grid = np.array(self._full_obs)
 
-        agent_pos = []
+        self.body = []
         for agent_id in range(self.n_agents):
-            tag = f"A{agent_id + 1}"
-            row, col = np.where(current_grid == tag)
-            row = row[0]
-            col = col[0]
-            agent_pos.append((col, row))
+            if self.alive[agent_id]:
+                tag = f"A{agent_id + 1}"
+                row, col = np.where(current_grid == tag)
+                row = row[0]
+                col = col[0]
+                self.body[agent_id].append((col, row))
 
-        features = np.array(agent_pos).reshape(-1)
+        features = np.array(self.body).reshape(-1)
 
         return features
 
     def reset(self):
-        self.agent_pos = {}
+        self.body = {}
         self.prey_pos = {}
 
         self.__init_full_obs()
         self._step_count = 0
         self._agent_dones = [False for _ in range(self.n_agents)]
+        self.alive = [True for _ in range(self.n_agents)]
         
         self.get_agent_obs()
         return [self.simplified_features() for _ in range(self.n_agents)]
+    
+    def __regenerate_food(self):
+        area_total = self._grid_shape[0] * self._grid_shape[1]
+        grid_players = list(functools.reduce(lambda a, b: a + b, self.agent_body))
+        area_available = area_total - len(grid_players)
+        food_target = self._food_equilibrium_f(area_available)
+        if len(self.food) > food_target:
+            return
+        sample = self.np_random.uniform(0, food_target)
+        if sample > food_target - len(self.food):
+            return
+        else:
+            # Spawn a food block in any random free space
+            while True:
+                x = math.floor(self.np_random.uniform(3, self._grid_shape[0] - 3 + 1))
+                y = math.floor(self.np_random.uniform(3, self._grid_shape[1] - 3 + 1))
+                if (x, y) in grid_players or (x, y) in self.food:
+                    continue
+                else:
+                    self.food += [(x, y)]
+                    return
+
+    def __create_snakes(self):
+        for a in self.agent_body:
+            x = math.floor(self.np_random.uniform(3, self._grid_shape[0] + 1))
+            y = math.floor(self.np_random.uniform(3, self._grid_shape[1] + 1))
+            a.append((x, y))
 
     def step(self, agents_action):
         self._step_count += 1
@@ -85,7 +129,7 @@ class SimplifiedPredatorPrey(gym.Env):
             if not (self._agent_dones[agent_i]):
                 self.__update_agent_pos(agent_i, action)
 
-        if (self._step_count >= self._max_steps) or (True not in self._prey_alive):
+        if (self._step_count >= self._max_steps) or (sum(self.alive)<2):
             for i in range(self.n_agents):
                 self._agent_dones[i] = True
 
@@ -93,6 +137,8 @@ class SimplifiedPredatorPrey(gym.Env):
             self._total_episode_reward[i] += rewards[i]
 
         self.get_agent_obs()
+
+        self.__regenerate_food()
 
         return [self.simplified_features() for _ in range(self.n_agents)], rewards, self._agent_dones
 
@@ -121,25 +167,16 @@ class SimplifiedPredatorPrey(gym.Env):
                 pos = [self.np_random.randint(0, self._grid_shape[0] - 1),
                        self.np_random.randint(0, self._grid_shape[1] - 1)]
                 if self._is_cell_vacant(pos):
-                    self.agent_pos[agent_i] = pos
+                    self.body[agent_i] = pos
                     break
             self.__update_agent_view(agent_i)
-
-        for prey_i in range(self.n_preys):
-            while True:
-                pos = [self.np_random.randint(0, self._grid_shape[0] - 1),
-                       self.np_random.randint(0, self._grid_shape[1] - 1)]
-                if self._is_cell_vacant(pos) and (self._neighbour_agents(pos)[0] == 0):
-                    self.prey_pos[prey_i] = pos
-                    break
-            self.__update_prey_view(prey_i)
 
         self.__draw_base_img()
 
     def get_agent_obs(self):
         _obs = []
         for agent_i in range(self.n_agents):
-            pos = self.agent_pos[agent_i]
+            pos = self.body[agent_i]
             _agent_i_obs = [pos[0] / (self._grid_shape[0] - 1), pos[1] / (self._grid_shape[1] - 1)]  # coordinates
 
             # check if prey is in the view area
@@ -170,7 +207,7 @@ class SimplifiedPredatorPrey(gym.Env):
 
     def __update_agent_pos(self, agent_i, move):
 
-        curr_pos = copy.copy(self.agent_pos[agent_i])
+        curr_pos = copy.copy(self.body[agent_i])
         next_pos = None
         if move == 0:  # down
             next_pos = [curr_pos[0] + 1, curr_pos[1]]
@@ -184,7 +221,7 @@ class SimplifiedPredatorPrey(gym.Env):
             raise Exception('Action Not found!')
 
         if next_pos is not None and self._is_cell_vacant(next_pos):
-            self.agent_pos[agent_i] = next_pos
+            self.body[agent_i] = next_pos
             self._full_obs[curr_pos[0]][curr_pos[1]] = PRE_IDS['empty']
             self.__update_agent_view(agent_i)
 
@@ -197,87 +234,39 @@ class SimplifiedPredatorPrey(gym.Env):
             next_pos = [curr_pos[0] - 1, curr_pos[1]]
         elif move == 3:  # right
             next_pos = [curr_pos[0], curr_pos[1] + 1]
-        elif move == 4:  # no-op
-            next_pos = curr_pos
         return next_pos
 
-    def __update_prey_pos(self, prey_i, move):
-        curr_pos = copy.copy(self.prey_pos[prey_i])
-        if self._prey_alive[prey_i]:
-            next_pos = None
-            if move == 0:  # down
-                next_pos = [curr_pos[0] + 1, curr_pos[1]]
-            elif move == 1:  # left
-                next_pos = [curr_pos[0], curr_pos[1] - 1]
-            elif move == 2:  # up
-                next_pos = [curr_pos[0] - 1, curr_pos[1]]
-            elif move == 3:  # right
-                next_pos = [curr_pos[0], curr_pos[1] + 1]
-            elif move == 4:  # no-op
-                pass
-            else:
-                raise Exception('Action Not found!')
-
-            if next_pos is not None and self._is_cell_vacant(next_pos):
-                self.prey_pos[prey_i] = next_pos
-                self._full_obs[curr_pos[0]][curr_pos[1]] = PRE_IDS['empty']
-                self.__update_prey_view(prey_i)
-            else:
-                # print('pos not updated')
-                pass
-        else:
-            self._full_obs[curr_pos[0]][curr_pos[1]] = PRE_IDS['empty']
-
     def __update_agent_view(self, agent_i):
-        self._full_obs[self.agent_pos[agent_i][0]][self.agent_pos[agent_i][1]] = PRE_IDS['agent'] + str(agent_i + 1)
+        self._full_obs[self.body[agent_i][0]][self.body[agent_i][1]] = PRE_IDS['agent'] + str(agent_i + 1)
 
-    def __update_prey_view(self, prey_i):
-        self._full_obs[self.prey_pos[prey_i][0]][self.prey_pos[prey_i][1]] = PRE_IDS['prey'] + str(prey_i + 1)
 
-    def _neighbour_agents(self, pos):
-        # check if agent is in neighbour
-        _count = 0
-        neighbours_xy = []
-        if self.is_valid([pos[0] + 1, pos[1]]) and PRE_IDS['agent'] in self._full_obs[pos[0] + 1][pos[1]]:
-            _count += 1
-            neighbours_xy.append([pos[0] + 1, pos[1]])
-        if self.is_valid([pos[0] - 1, pos[1]]) and PRE_IDS['agent'] in self._full_obs[pos[0] - 1][pos[1]]:
-            _count += 1
-            neighbours_xy.append([pos[0] - 1, pos[1]])
-        if self.is_valid([pos[0], pos[1] + 1]) and PRE_IDS['agent'] in self._full_obs[pos[0]][pos[1] + 1]:
-            _count += 1
-            neighbours_xy.append([pos[0], pos[1] + 1])
-        if self.is_valid([pos[0], pos[1] - 1]) and PRE_IDS['agent'] in self._full_obs[pos[0]][pos[1] - 1]:
-            neighbours_xy.append([pos[0], pos[1] - 1])
-            _count += 1
-
-        agent_id = []
-        for x, y in neighbours_xy:
-            agent_id.append(int(self._full_obs[x][y].split(PRE_IDS['agent'])[1]) - 1)
-        return _count, agent_id
-
-    def __get_neighbour_coordinates(self, pos):
-        neighbours = []
-        if self.is_valid([pos[0] + 1, pos[1]]):
-            neighbours.append([pos[0] + 1, pos[1]])
-        if self.is_valid([pos[0] - 1, pos[1]]):
-            neighbours.append([pos[0] - 1, pos[1]])
-        if self.is_valid([pos[0], pos[1] + 1]):
-            neighbours.append([pos[0], pos[1] + 1])
-        if self.is_valid([pos[0], pos[1] - 1]):
-            neighbours.append([pos[0], pos[1] - 1])
-        return neighbours
+    def _check_colisions(self):
+        auxAlive = copy.deepcopy(self.alive)
+        for agent_i in range(self.n_agents):
+            if self.alive[agent_i]:
+                for agent_x in range(self.n_agents):
+                    if self.alive[agent_x]:
+                        if agent_i == agent_x:
+                            continue
+                        if not self.is_valid(self.body[0]):
+                            auxAlive[agent_i] = False    
+                        if(self.body[agent_i][0] == self.body[agent_x][0]):
+                            auxAlive[agent_i] = False
+                            auxAlive[agent_x] = False
+                        if(self.body[agent_i][0] in self.body[agent_x]):
+                            auxAlive[agent_i] = False
+        self.alive = auxAlive 
+                
 
     def render(self, mode='human'):
         img = copy.copy(self._base_img)
         for agent_i in range(self.n_agents):
-            for neighbour in self.__get_neighbour_coordinates(self.agent_pos[agent_i]):
-                fill_cell(img, neighbour, cell_size=CELL_SIZE, fill=AGENT_NEIGHBORHOOD_COLOR, margin=0.1)
-            fill_cell(img, self.agent_pos[agent_i], cell_size=CELL_SIZE, fill=AGENT_NEIGHBORHOOD_COLOR, margin=0.1)
+            for cell in self.body[agent_i]:
+                fill_cell(img, cell, cell_size=CELL_SIZE, fill=self.getColor(agent_i,True), margin=0.1)
 
         for agent_i in range(self.n_agents):
-            draw_circle(img, self.agent_pos[agent_i], cell_size=CELL_SIZE, fill=AGENT_COLOR)
-            write_cell_text(img, text=str(agent_i + 1), pos=self.agent_pos[agent_i], cell_size=CELL_SIZE,
+            draw_circle(img, self.body[agent_i][0], cell_size=CELL_SIZE, fill=self.getColor(agent_i))
+            write_cell_text(img, text=str(agent_i + 1), pos=self.body[agent_i][0], cell_size=CELL_SIZE,
                             fill='white', margin=0.4)
 
         img = np.asarray(img)
@@ -299,10 +288,15 @@ class SimplifiedPredatorPrey(gym.Env):
             self.viewer.close()
             self.viewer = None
 
+    def getColor(agent_i, body=False):
+        color = ImageColor.getcolor(AGENT_COLOR[agent_i], mode='HSV')
+        if(body):
+            color[1] = 0.9
+        return color 
+        
 
-AGENT_COLOR = ImageColor.getcolor('blue', mode='RGB')
-AGENT_NEIGHBORHOOD_COLOR = (186, 238, 247)
-PREY_COLOR = 'red'
+AGENT_COLOR = ["red","blue","green","yellow","purple","orange","pink","brown"] #8 agent colors
+FOOD_COLOR = "black"
 
 CELL_SIZE = 35
 
