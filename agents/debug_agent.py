88cdcecab8776b4753a480b87a52802ef1eb1b7d
David Duque <david@duqued.net>
2023-06-01 07:31:35 +0100

    Implement debug agent

---
+++ agents/debug_agent.py
@@ -0,0 +1,10 @@
+# A set of agents for environment debugging purposes only.
+from aasma.agent import Agent
+from aasma.snake_environment.snake_environment import Action
+
+class ForwardAgent(Agent):
+    def __init__(self):
+        super(ForwardAgent, self).__init__("[Debug] Forward-Only Agent")
+    
+    def action(self) -> int:
+        return Action.FORWARD.value
