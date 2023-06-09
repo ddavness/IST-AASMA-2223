78e51b7b03dfd3c8eccafde28f195f3c32f2b7fc
David Duque <david@duqued.net>
2023-06-09 10:31:36 +0100

    byteorder may be mandatory in older python versions

--- data/utils.py
+++ data/utils.py
@@ -5,4 +5,4 @@ def mkhash(string: str):
     barray = string.encode("utf-8")
     m.update(barray)
     digest = m.digest()
-    return int.from_bytes(digest)
+    return int.from_bytes(digest, byteorder="big")
